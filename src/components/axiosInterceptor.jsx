549ce4e4a68df545508e6a4b43a427582f100e82
TungLam1410 <lam2002ttb@gmail.com>
2023-06-28 00:57:39 +0700

    handle login

---
+++ src/components/axiosInterceptor.jsx
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+// Tạo một instance Axios
+const api = axios.create({
+  baseURL: 'http://localhost:3001/api',
+});
+
+// Tạo một interceptor để xử lý tự động gia hạn AccessToken
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    // Kiểm tra xem lỗi có mã trạng thái 401 (Unauthorized) hay không
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        // Gửi yêu cầu tới API để lấy AccessToken mới
+        const response = await api.post('/auth/refresh');
+        const { accessToken } = response.data;
+
+        // Lưu AccessToken mới vào localStorage
+        localStorage.setItem('accessToken', accessToken);
+
+        // Gửi lại yêu cầu ban đầu với AccessToken mới
+        originalRequest.headers['token'] = `Bearer ${accessToken}`;
+        return api(originalRequest);
+      } catch (refreshError) {
+        // Xử lý lỗi khi không thể lấy AccessToken mới
+        console.error('Error refreshing access token:', refreshError);
+        // Chuyển hướng đến trang đăng nhập hoặc hiển thị thông báo lỗi
+      }
+    }
+
+    // Trả về lỗi ban đầu nếu không cần gia hạn AccessToken
+    return Promise.reject(error);
+  }
+);
+
+export default api;
---
+++ src/components/userContext.jsx
@@ -0,0 +1,53 @@
+import React, { createContext, useEffect, useState } from 'react';
+import api from './axiosInterceptor';
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    const userID = localStorage.getItem('userID');
+    if (accessToken && userID) {
+      api.get(`http://localhost:3001/api/user/${userID}`, {
+        headers: { token: `Bearer ${accessToken}` },
+      })
+      .then(response => {
+        const user = response.data;
+        if (user) {
+          setUser(user);
+        } else {
+          setUser(null);
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching user data:', error);
+      })
+      .finally(() => {
+        setLoading(false); // Kết thúc quá trình loading
+      });
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+  const login = (user) => {
+    setUser(user);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <UserContext.Provider value={{ user, login, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
